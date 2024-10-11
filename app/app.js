95953c0a76e33578125570dc8ab3d80580c2c805
Slava Homenko <hosl_boy@inbox.ru>
2024-10-11 15:50:39 +0300

    Меню бургер

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@ const isMobile = {
             isMobile.Windows());
     }
 };
-
+// --------------------
 if (isMobile.any()){
     document.body.classList.add('_touch');
 
@@ -40,3 +40,35 @@ if (isMobile.any()){
 }else{
     document.body.classList.add('_pc');
 }
+//Бургер
+const iconMenu = document.querySelector('.menu__icon');
+if(iconMenu){
+    const menuRight = document.querySelector('.content-right__nav-bar');
+    iconMenu.addEventListener('click',function(){
+        iconMenu.classList.toggle('_active');
+        menuRight.classList.toggle('_active');
+    });
+}
+
+
+// --------------------
+// const menuLinks = document.querySelectorAll('.nav-bar__item[data-goto]');
+// if(menuLinks.length>0){
+//     menuLinks.forEach(menuLink=>{
+//         menuLink.addEventListener('click',onMenuLinkClick);
+//     });
+
+//     function onMenuLinkClick(e){
+//         const menuLink = e.target;
+//         if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+//             const gotoBlock = document.querySelector(menuLink.dataset.goto);
+//             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+        
+//             window.scrollTo({
+//                 top: gotoBlockValue,
+//                 behavior: "smooth"
+//             });
+//             e.preventDefault();
+//         }
+//     }
+// }
