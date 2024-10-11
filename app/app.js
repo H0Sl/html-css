2ea502ef0f2d89c4322a8343bb7b139675ddcd02
Slava Homenko <hosl_boy@inbox.ru>
2024-10-11 13:58:38 +0300

    На mobile > 768px in nav добавил click screen

--- app/app.js
+++ app/app.js
@@ -26,6 +26,17 @@ const isMobile = {
 
 if (isMobile.any()){
     document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if (menuArrows.length>0){
+        for(let index = 0;index < menuArrows.length;index++){
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click",function(){
+                menuArrow.parentElement.classList.toggle('_active')
+            });
+        }
+    }
+
 }else{
     document.body.classList.add('_pc');
 }
